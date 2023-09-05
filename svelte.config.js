719a3008301edaa50bdcead0077df2992abfc453
itsarunkumar <003arunkumark@gmail.com>
2023-09-05 15:00:31 +0530

    adapter update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from 'svelte-adapter-deno';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
